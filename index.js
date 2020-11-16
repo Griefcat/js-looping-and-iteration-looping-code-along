7bb90fec43c5df9dbd88f7b761a5df3a579db4ae
Griefcat <anardolilli@gmail.com>
2020-11-15 21:03:30 -0500

    function fun

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+names = ["Ada", "Brendan", "Ali"];
+function writeCards(names) {
+    for (let i = 0; i < names.length; i ++) {
+        console.log(`Thank you, ${names[i]} for the wonderful birthday gift`)
+    }
+    return names
+}
+
+function countDown(num) {
+    let count = num;
+    while (count >= 0) {
+      console.log(count --)
+    }
+  }
+  
+  countDown(13)
